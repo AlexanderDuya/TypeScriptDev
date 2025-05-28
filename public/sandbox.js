347d548e002af648326233777ccacd3db084613d
AlexanderDuya <alexduyaconnect@gmail.com>
2025-05-28 22:35:47 +0100

    function basics

--- public/sandbox.js
+++ public/sandbox.js
@@ -8,7 +8,7 @@ console.log(inputs);
 inputs.forEach((input) => {
     console.log(input);
 });
-// i can do tsc sandbox.ts -w , so everytime i make a change it will recompile but i will do it manually for a bit
+// i can do tsc sandbox.ts -w , or tsc -wso everytime i make a change it will recompile but i will do it manually for a bit
 //Part 2
 // this can take any type because its not specified
 const circ = (diameter) => {
@@ -101,3 +101,36 @@ dynamicNinja = { name: "burger", age: 2 };
 console.log(dynamicNinja);
 // Part 7 Better Workflow & tsconfig
 console.log("testing");
+// add this to bottom of tsconfig.json "include": ["src"]
+// uncomment and add "outDir": "./public" and "rootDir": "./src"
+// basically saying that I want any ts file in src to be converted to js in the public file for the browser.
+// to get tsconfig.json do tsc --init. do tsc -w to not have to save
+// src is for ts and public is for js
+//Part 8 Function Basics
+let greet = () => {
+    console.log("hello there");
+};
+console.log(greet);
+//we can do
+let greet2;
+greet2 = () => {
+    console.log("I replaced the greet");
+};
+console.log(greet2);
+greet2();
+const add = (a, b, c) => {
+    console.log(a + b);
+};
+add(5, 10);
+//what if i wanted an optional parameter
+//all you do is add it then put ? and u can still say if its more than 1 type for example number or string by using the pipe
+//default parameter by just using = . use either = or ?.
+//if yo use ? then it will beundefined if you use = not ? then it will be what you assign it
+// this is like explicity doing:void if no return type
+const minus = (a, b) => {
+    return a + b;
+};
+let result = minus(10, 7);
+// result wil be the type of what has been returned in the function
+// to explicity declare the type add a colon like normal. but we dont have to do it but it might be good
+// good for other developers
